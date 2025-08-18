dd74c510591f4672f89afc3e5c17358d5eeb067b
Dhaval Arya <dhaval.arya@gep.com>
2025-08-18 13:00:38 +0530

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
     deviceSizes: [360, 414, 640, 768, 1024, 1280], // good spread for phones/tablets
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  swcMinify: true,
   poweredByHeader: false,
 };
 
